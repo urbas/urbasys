f5a0218b4bf5572aba7dbafe052cd31a4c2fd9fd
Matej Urbas <matej.urbas@gmail.com>
2022-12-23 13:11:35 +0000

    port to nix and github actions

--- setup.py
+++ setup.py
@@ -5,11 +5,6 @@
 
 from setuptools import setup, find_packages
 
-REQUIREMENTS = ["click==7.0", "requests==2.22.0"]
-
-SETUP_REQUIREMENTS = ["pytest-runner"]
-
-TEST_REQUIREMENTS = ["pytest"]
 
 setup(
     author="Matej Urbas",
@@ -17,16 +12,12 @@ setup(
     description="A collection of applications to support the Urbas System.",
     entry_points={
         "console_scripts": [
-            "urbasys=urbasys.app:main",
             "urbackup=urbasys.urbackup.app:main",
         ]
     },
-    install_requires=REQUIREMENTS,
     include_package_data=True,
     keywords="urbasys",
     name="urbasys",
     packages=find_packages(include=["urbasys"]),
-    setup_requires=SETUP_REQUIREMENTS,
     test_suite="tests",
-    tests_require=TEST_REQUIREMENTS,
 )
